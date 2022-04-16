7d6e6da5fbb50e92ed6a37acab9387906df8aa7b
Juliette Lelchuk <juliette.lelchuk@gmail.com>
2022-04-16 15:00:29 -0500

    add MaterialTable

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
+import MaterialTable from 'material-table';
+import React, {useState} from 'react';
+
+
+// Basic set up, using JSON Placeholder: https://www.youtube.com/watch?v=S_mgSHCWCmA
+
+// CRUD operations in data grid: https://www.youtube.com/watch?v=S7-99HqpWvo
+const donations = [
+  {
+    "date": "1/1/2022",
+    "org name": "World Street Kitchen",
+    "amount": "$25",
+    "sent": false
+  }
+]
+
 
 function App() {
+
+  const [setData, setNewData] = useState(donations);
+
+  const columns = [
+    { date: "Date", field: "date" },
+    { date: "Organization", field: "org name" },
+    { date: "Dollar Amount", field: "amount" },
+    { date: "Sent?", field: "sent" }
+  ]
+console.log(donations);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Data Grid SPIKE!</h1>
+      <MaterialTable
+        title="Donations"
+        data={setData}
+        columns={columns}
+
+
+
+      />
     </div>
   );
 }
