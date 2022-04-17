bd8342bb2ebb6e29dfe3b5e7da3d0e766e581d70
Juliette Lelchuk <juliette.lelchuk@gmail.com>
2022-04-16 21:16:25 -0500

    comment code with youtube links

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
 import React, { useState } from 'react';
 import { DataGrid, GridRowsProp, GridColDef } from '@mui/x-data-grid';
-import { Checkbox, Button } from '@mui/material';
+// import { Checkbox, Button } from '@mui/material';
 
 
-// Basic set up of Material Table, using JSON Placeholder: https://www.youtube.com/watch?v=S_mgSHCWCmA
+/* 
+Unsuccessful 😢
+Basic set up of Material Table, using JSON Placeholder: 
+https://www.youtube.com/watch?v=S_mgSHCWCmA
 
-// CRUD operations in Material Table: https://www.youtube.com/watch?v=S7-99HqpWvo
+CRUD operations in Material Table: 
+https://www.youtube.com/watch?v=S7-99HqpWvo
+
+*/
 
 
 
@@ -54,7 +60,7 @@ const columns: GridColDef[] = [
   {
     field: 'sent',
     // renderEditCell: (<Checkbox checked={checked} onChange={handleChange}/>),
-    //for checkbox
+    //hmm further research needed re: adding 'sent?' checkbox column w/ boolean 
     headerName: 'Sent?',
     width: 150,
     editable: true
@@ -64,11 +70,17 @@ const columns: GridColDef[] = [
 
 
 /* Make cells editable and save to state: 
-https://www.youtube.com/watch?v=niafDCOT_uA
+  https://www.youtube.com/watch?v=niafDCOT_uA
 
-Data grid - fetching data from API, loading, error messages:
-https://www.youtube.com/watch?v=5sEXClpvL34
+Data grid - fetching data from API, loading, error messages,
+adjusting pagination / updating useEffect
+  https://www.youtube.com/watch?v=5sEXClpvL34
+
+Delete Multiple Rows Using Checkbox in React w/ Redux:
+  https://www.youtube.com/watch?v=9MFz-ApAPp4
+hooking up to redux ~ 4:00
 */
+
 function App() {
 
   // const [checked, setChecked] = React.useState(true); //for checkbox
@@ -98,47 +110,45 @@ function App() {
       if (r.id === e.id) { //compares the record id to the edit id
         return { ...r, [e.field]: e.value } //if matching, replaces the old value
       } else {
-        return { ...r } //returns the array without changes
+        return { ...r } //if not saved, returns the array without changes
       }
     });
     setState(array); //sets state to changed / unchanged values
   }
 
-  const [arrIds, setArrIds] = useState([]);// state for selected rows
-  const handleDeleteAll = () => { 
+  const [arrIds, setArrIds] = useState([]);// for delete: state for selected rows
+  const handleDeleteAll = () => {
     //will need to dispatch to a Saga to set a reducer,
     //send delete query etc 
     console.log(arrIds);//shows ids of checkbox selected items
   }
-  /* Delete Multiple Rows Using Checkbox in React w/ Redux:
-  https://www.youtube.com/watch?v=9MFz-ApAPp4
-  hooking up to redux ~ 4:00
-  */
+
 
   return (
-    <div className="App">
-      <h1>Data Grid SPIKE!</h1>
-      <div>{JSON.stringify(state)}</div>
+    <div style={{alignItems:'center'}} className="App">
+      <h1>Data Grid SPIKE!📌</h1>
+      <div style={{fontSize: 20}}>{JSON.stringify(state)}</div>
 
       <button onClick={handleDeleteAll}
-        style={{ backgroundColor: 'red', color: 'white' }}>
+        style={{ height: '30px', backgroundColor: 'red', color: 'white' }}>
         Delete selected rows
       </button>
 
-      <div style={{ height: 300, width: '100%' }}>
+      <div style={{ display: '-ms-grid', height: 300, width: '50%'}}>
         <DataGrid onCellEditCommit={handleCommit}
           checkboxSelection
           rows={rowData} columns={columns}
           getRowId={(rowData) => rowData.id}
-          onSelectionModelChange={(ids) => {
+          onSelectionModelChange={(ids) => {//for grabbing ids of checked rows
             setArrIds(ids);
           }}
+          style={{fontSize: 20}}
         />
       </div>
       {/* <Button variant="contained" color="primary" onClick={handlePurge}>
         Purge
       </Button> 
-      from batch delete attempt*/}
+      From abandoned batch delete attempt*/}
     </div>
   );
 }
