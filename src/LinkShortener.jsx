97b9e2958ef0c8de38d3ec988102e60de2de7321
Juliette Lelchuk <juliette.lelchuk@gmail.com>
2022-05-14 23:35:40 -0500

    install use-copy

--- src/LinkShortener.jsx
+++ src/LinkShortener.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 import axios from 'axios';
 import { useSelector, useDispatch } from 'react-redux';
+import useCopy from "use-copy";
+
 
 
 
@@ -12,18 +14,32 @@ function LinkShortener() {
     const dispatch = useDispatch();
     const URL = useSelector((store) => store.URLreducer);
 
+    /******* useCopy
+    
+    const [text, setText] = useState(randomStrings[0]);
+  const [copied, copy, setCopied] = useCopy(text);
+ 
+  const copyText = () => {
+    copy();
+
+    setTimeout(() => {
+      setCopied(false);
+    }, 3000);
+  };
+     
+    ********/
 
     const shortenURL = (event) => {
         event.preventDefault();
         console.log('url/input text on client side is:', longURL);
         axios.post(`/link`, longURL)
-      .then(response => {
-        console.log(response.data);
-        // dispatch({ type: 'SET_URL', payload: response.data })
-      })
-      .catch( error => {
-        console.log(error);
-      });
+            .then(response => {
+                console.log(response.data);
+                // dispatch({ type: 'SET_URL', payload: response.data })
+            })
+            .catch(error => {
+                console.log(error);
+            });
     }
 
 
@@ -33,7 +49,20 @@ function LinkShortener() {
             <form onSubmit={shortenURL}>
                 <input value={longURL} type="text" onChange={(event) => setLongURL(event.target.value)} />
             </form>
-            <h1>The shortened link is: </h1>
+            <h3>The shortened link is: </h3>
+
+            {/* <div>
+                <p>The text we're gonna copy is:</p>
+                <h2>{text}</h2>
+                {copied ? (
+                    <p>Text copied to clipboard</p>
+                ) : (
+                    <button onClick={copyText}>Copy text</button>
+                )}
+                <br />
+            </div> */}
+
+
         </>
     )
 }
